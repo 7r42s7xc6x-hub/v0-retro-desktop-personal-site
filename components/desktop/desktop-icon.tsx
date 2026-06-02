71994f0be94c6571acc51bbd48a752d75e95e77b
v0agent <it+v0agent@vercel.com>
2026-06-02 21:37:50 +0000

    feat: redesign desktop icon to rolodex style

--- components/desktop/desktop-icon.tsx
+++ components/desktop/desktop-icon.tsx
@@ -335,25 +335,24 @@ export function DesktopIcon({
         )
       case "people":
         return (
-          <div className="w-14 h-11 relative">
-            {/* Folder tab */}
-            <div className="absolute top-0 left-1 w-5 h-3 bg-[#d4a574] border-2 border-b-0 border-[#2a2a2a] rounded-t-sm" />
-            {/* Folder body */}
-            <div className="absolute top-2 left-0 right-0 bottom-0 bg-[#d4a574] border-2 border-[#2a2a2a] rounded-sm flex items-center justify-center gap-0.5 pt-1">
-              {/* Three people silhouettes */}
-              <div className="flex items-end -mr-1">
-                <div className="w-1.5 h-1.5 bg-[#2a2a2a] rounded-full" />
-                <div className="w-1.5 h-2.5 bg-[#2a2a2a] rounded-t-sm -ml-0.5" />
-              </div>
-              <div className="flex items-end z-10">
-                <div className="w-2 h-2 bg-[#2a2a2a] rounded-full" />
-                <div className="w-2 h-3 bg-[#2a2a2a] rounded-t-sm -ml-0.5" />
-              </div>
-              <div className="flex items-end -ml-1">
-                <div className="w-1.5 h-1.5 bg-[#2a2a2a] rounded-full" />
-                <div className="w-1.5 h-2.5 bg-[#2a2a2a] rounded-t-sm -ml-0.5" />
-              </div>
+          <div className="w-14 h-12 relative">
+            {/* Rolodex base/stand */}
+            <div className="absolute bottom-0 left-1 right-1 h-2 bg-[#5a5a5a] border-2 border-[#2a2a2a] rounded-sm" />
+            {/* Left spindle */}
+            <div className="absolute bottom-1 left-2 w-1.5 h-9 bg-[#4a4a4a] border border-[#2a2a2a] rounded-t-full" />
+            {/* Right spindle */}
+            <div className="absolute bottom-1 right-2 w-1.5 h-9 bg-[#4a4a4a] border border-[#2a2a2a] rounded-t-full" />
+            {/* Back cards (fanned) */}
+            <div className="absolute bottom-3 left-3 right-3 h-5 bg-[#d4c9a8] border border-[#2a2a2a] rounded-t-sm -rotate-6 origin-bottom" />
+            <div className="absolute bottom-3 left-3 right-3 h-5 bg-[#e0d5b4] border border-[#2a2a2a] rounded-t-sm -rotate-3 origin-bottom" />
+            {/* Front card */}
+            <div className="absolute bottom-3 left-3 right-3 h-6 bg-[#f5f0dc] border-2 border-[#2a2a2a] rounded-t-sm">
+              {/* Card lines */}
+              <div className="absolute top-1.5 left-1 right-1 h-[1px] bg-[#2a2a2a] opacity-30" />
+              <div className="absolute top-3 left-1 right-2 h-[1px] bg-[#2a2a2a] opacity-30" />
             </div>
+            {/* Top knob */}
+            <div className="absolute top-0 left-1/2 -translate-x-1/2 w-3 h-2 bg-[#6a6a6a] border border-[#2a2a2a] rounded-t-full" />
           </div>
         )
       default:
